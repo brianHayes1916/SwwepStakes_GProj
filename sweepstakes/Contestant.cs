6b1a8dfec11087fb18e1a68cdbd7b2b18f1c8d07
Brian Hayes <briancampbellhayes@icloud.com>
2021-03-03 11:17:52 -0600

    Contestant class given variables. All other classes empty but created.

---
+++ sweepstakes/Contestant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sweepstakes
+{
+    public class Contestant
+    {
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public int registrationNumber;
+
+        public Contestant(string fname, string lname, string email, int number)
+        {
+            firstName = fname;
+            lastName = lname;
+            emailAddress = email;
+            registrationNumber = number;
+        }
+
+    }
+}
