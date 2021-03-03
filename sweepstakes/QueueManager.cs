55dab53786bc668752408aec7c8cfd7549a5f3c6
Brian Hayes <briancampbellhayes@icloud.com>
2021-03-03 15:05:43 -0600

    queue manager constructor complete.

---
+++ sweepstakes/QueueManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sweepstakes
+{
+    class QueueManager : ISweepstakesManager
+    {
+        private Queue<SweepStakes> sweepStakes;
+
+        public QueueManager()
+        {
+            sweepStakes = new Queue<SweepStakes>();
+        }
+
+        public SweepStakes GetSweepStakes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertSweepstakes(SweepStakes sweepStakes)
+        {
+
+        }
+    }
+}
