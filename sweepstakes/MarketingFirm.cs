2d4812cd5bcf6111921d42bcadf9717bbdf7d093
Brian Hayes <briancampbellhayes@icloud.com>
2021-03-04 12:11:06 -0600

    The Marketing firm now has dependancy.

---
+++ sweepstakes/MarketingFirm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sweepstakes
+{
+    class MarketingFirm
+    {
+        private ISweepstakesManager manager;
+
+        public MarketingFirm(ISweepstakesManager sweepstakesManager)
+        {
+            manager = sweepstakesManager;
+        }
+
+        public void CreateSweepstakes()
+        {
+
+        } 
+    }
+}
