b5572edc8c5066271c41bcaf7f787dad4e49b5bd
Brian Hayes <briancampbellhayes@icloud.com>
2021-03-04 16:42:41 -0600

    Simulation class has functionality of creating a Marketing firm just needs UI.

--- sweepstakes/Simulation.cs
+++ sweepstakes/Simulation.cs
@@ -8,5 +8,30 @@ namespace sweepstakes
 {
     class Simulation
     {
+        public void CreateMarketingFirm()
+        {
+            bool validChoice = false;
+            while (validChoice == false)
+            {
+                Console.WriteLine("queue = 1 stack = 2");
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    QueueManager queueManager = new QueueManager();
+                    MarketingFirm marketingFirm = new MarketingFirm(queueManager);
+                    validChoice = true;
+                }
+                else if (input == "2")
+                {
+                    StackManager stackManager = new StackManager();
+                    MarketingFirm marketingFirm = new MarketingFirm(stackManager);
+                    validChoice = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input try again.");
+                }
+            }
+        }
     }
 }
