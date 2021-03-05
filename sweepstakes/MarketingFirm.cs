97ba22e393cc2eb06a1e02c664d0a36c686b0942
Brian Hayes <briancampbellhayes@icloud.com>
2021-03-05 08:33:10 -0600

    final

--- sweepstakes/MarketingFirm.cs
+++ sweepstakes/MarketingFirm.cs
@@ -12,6 +12,8 @@ namespace sweepstakes
 
         public MarketingFirm(ISweepstakesManager sweepstakesManager)
         {
+            //Dependacy starts in the constructor by saying we need a class that is a ISweepstakesManager
+            //to be instasiated
             manager = sweepstakesManager;
         }
 
@@ -20,6 +22,9 @@ namespace sweepstakes
             string sweepName = UserInterface.CreateSweepstakesName();
             SweepStakes sweepStakes = new SweepStakes(sweepName);
             manager.InsertSweepstakes(sweepStakes);
+            //This is really where the dependancy injection is evident
+            //All IsweepstakesManagers have this method so even though stack and queue add values very differntly
+            //they are interchangable here.
         } 
     }
 }
